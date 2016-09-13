1fb7f6625e9a2e0911dbcdecbe650d0abf0f751d
Patrick Mead <patrick.mead@dubitlimited.com>
2016-09-13 10:58:28 +0100

    Add post to teamcity build

--- src/server.js
+++ src/server.js
@@ -1,17 +1,53 @@
 import path from 'path';
 import koa from 'koa';
 import koaRouter from 'koa-router';
+import bodyParser from 'koa-bodyparser'
+import request from 'koa-request';
 
-const PORT = process.env.PORT || 8080;
+// Configuration
+const teamcity_url = process.env.TEAMCITY_URL;
+const teamcity_user = process.env.TEAMCITY_USER;
+const teamcity_pass = process.env.TEAMCITY_PASS;
+const serverPort = process.env.PORT || 8080;
+
 const app = koa();
 const router = koaRouter();
 
+app.use(bodyParser());
+
 // Routes
-router.get('/', function *(next) {
-  this.body = 'Unity Cloud Build Processor';
-});
+router.post('/build', function*(next) {
+    let data = this.request.body;
+    this.body = data;
+
+    var payload = JSON.stringify({
+      'buildType': {
+        'id': 'insert_build_id'
+      }
+    });
+
+    var options = {
+      auth: {
+        'user': teamcity_user,
+        'pass': teamcity_pass
+      },
+      url: teamcity_url,
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'request',
+        'Content-Type': 'application/json'
+      },
+      body: payload
+    };
+
+    var response = yield request(options);
+
+    console.log(response.body);
+    this.body = response.body;
+  });
 
 app.use(router.routes());
-app.listen(PORT);
+app.listen(serverPort);
 
-console.log(`listening on port ${PORT}`);
+console.log(`Listening on port ${serverPort}`);
