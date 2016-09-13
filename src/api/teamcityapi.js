ac6c4727ef5c32e941fefb47eb9a430373bdf555
Patrick Mead <patrick.mead@dubitlimited.com>
2016-09-13 11:29:10 +0100

    Move build request into a teamcity api module

---
+++ src/api/teamcityapi.js
@@ -0,0 +1,33 @@
+import request from 'koa-request';
+
+const teamcity_url = process.env.TEAMCITY_URL;
+const teamcity_user = process.env.TEAMCITY_USER;
+const teamcity_pass = process.env.TEAMCITY_PASS;
+
+export default class TeamCityAPI {  
+
+  static createBuild(build_id) {
+    let payload = JSON.stringify({
+      'buildType': {
+        'id': build_id
+      }
+    });
+
+    var options = {
+      auth: {
+        'user': teamcity_user,
+        'pass': teamcity_pass
+      },
+      url: teamcity_url,
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'request',
+        'Content-Type': 'application/json'
+      },
+      body: payload
+    };
+
+    return request(options);    
+  }
+}
--- src/server.js
+++ src/server.js
@@ -2,12 +2,9 @@ import path from 'path';
 import koa from 'koa';
 import koaRouter from 'koa-router';
 import bodyParser from 'koa-bodyparser'
-import request from 'koa-request';
 
-// Configuration
-const teamcity_url = process.env.TEAMCITY_URL;
-const teamcity_user = process.env.TEAMCITY_USER;
-const teamcity_pass = process.env.TEAMCITY_PASS;
+import teamcityapi from './api/teamcityapi';
+
 const serverPort = process.env.PORT || 8080;
 
 const app = koa();
@@ -16,36 +13,11 @@ const router = koaRouter();
 app.use(bodyParser());
 
 // Routes
-router.post('/build', function*(next) {
-    let data = this.request.body;
-    this.body = data;
+router.post('/build', function* (next) {
+  let response = yield teamcityapi.createBuild('build_id');
 
-    var payload = JSON.stringify({
-      'buildType': {
-        'id': 'insert_build_id'
-      }
-    });
-
-    var options = {
-      auth: {
-        'user': teamcity_user,
-        'pass': teamcity_pass
-      },
-      url: teamcity_url,
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'User-Agent': 'request',
-        'Content-Type': 'application/json'
-      },
-      body: payload
-    };
-
-    var response = yield request(options);
-
-    console.log(response.body);
-    this.body = response.body;
-  });
+  this.body = response.body;
+});
 
 app.use(router.routes());
 app.listen(serverPort);
